ea76ddd93d403c38f654dedeac88c7b691a098c7
kamissvitoria <kmorssantos@gmail.com>
2025-04-10 12:35:46 -0300

    "Implementando model, migration, livewire index e seeder do cliente"

---
+++ app/Models/Cliente.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'nome',
+        'endereco',
+        'telefone',
+        'cpf',
+        'email',
+        'senha'
+    ];
+
+    protected $hidden = [
+        'senha',
+        'remember_token'
+    ];
+}
